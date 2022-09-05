c133e2f2b2d7db0f087a761fe72f7e701359079b
Luiz Junior <luiz.sinf@gmail.com>
2022-09-05 18:42:01 -0300

    Product entity, SQL script

--- backend/dscatalog/src/main/java/com/desvsuperior/dscatalog/dscatalog/entities/Category.java
+++ backend/dscatalog/src/main/java/com/desvsuperior/dscatalog/dscatalog/entities/Category.java
@@ -1,22 +1,35 @@
 package com.desvsuperior.dscatalog.dscatalog.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 @Entity
-@Table(name="tb_category")
+@Table(name="tb_category" )
 public class Category implements Serializable {
 	private static final long serialVersionUID =1L;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	private String name;
 	
+	
+    @Column(columnDefinition ="TIMESTAMP WITHOUT TIME ZONE")
+	private Instant created_At;
+	
+
+	@Column(columnDefinition ="TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
+	
+	
+
 	public Long getId() {
 		return id;
 	}
@@ -29,7 +42,7 @@ public class Category implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
-	private String name;
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
