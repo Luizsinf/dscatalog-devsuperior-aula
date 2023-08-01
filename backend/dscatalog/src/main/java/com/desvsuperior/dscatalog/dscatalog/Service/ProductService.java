df882b02fbc0d44b2fa2a5bcd7024ea50475ec62
Luiz Junior <luiz.sinf@gmail.com>
2023-08-01 15:34:07 -0300

    Refactor pageable

--- backend/dscatalog/src/main/java/com/desvsuperior/dscatalog/dscatalog/Service/ProductService.java
+++ backend/dscatalog/src/main/java/com/desvsuperior/dscatalog/dscatalog/Service/ProductService.java
@@ -1,5 +1,6 @@
 package com.desvsuperior.dscatalog.dscatalog.Service;
 
+
 import java.util.Optional;
 
 
@@ -10,6 +11,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -36,8 +38,8 @@ public class ProductService {
 	private CategoryRepository categoryRepository;
 	
 	@Transactional(readOnly =true)
-		public Page<ProductDTO> findAllPaged(PageRequest pageRequest){
-			Page<Product> list = repository.findAll(pageRequest);
+		public Page<ProductDTO> findAllPaged(Pageable pageable){
+			Page<Product> list = repository.findAll(pageable);
 			return  list.map(X ->  new ProductDTO(X));
 	}
 
